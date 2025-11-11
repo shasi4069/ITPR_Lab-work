6ef22f3e905a7340fd6092f2ebb61c5156ed7a34
Shasi <shasi4069@gmail.com>
2025-11-11 14:56:45 +0530

    Basic Arithematic Operatiion

--- 3 October/TriangleBasedOnAngles.java
+++ 3 October/TriangleBasedOnAngles.java
@@ -1,8 +1,10 @@
+/*--- Program to check three angles form a triangle or not 
+ * -----------------------------------------------------------------
+ * -- if they form a triangle then determine the type of triangle being formed------*/
 import java.util.Scanner;
-public class Triangle {
+public class TriangleBasedOnAngles {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		float angle1,angle2,angle3;
 		//create object of scanner class
 		Scanner sc=new Scanner(System.in);
@@ -20,12 +22,26 @@ public class Triangle {
 		/*--- Verify Triangle formation */
 		if(angle1 > 0 && angle2 > 0 && angle3 > 0 && (angle1 + angle2 + angle3) == 180)
 		{
-			System.out.println("These angles form a triangle");
+			//triangle formed
+			if(angle1<90 && angle2<90 && angle3<90) //Acute angled triangle
+			{
+				System.out.println("Above angles form acute angled triangle");
+			}
+			else
+			{
+				if(angle1==90 || angle2==90 || angle3==90)//right angled triangle
+				{
+					System.out.println("Above angles form right angled triangle");
+				}
+				else
+				{
+					System.out.println("Above angles form Obtuse angled triangle");
+				}
+			}
 		}
 		else
 		{
 			System.out.println("These angles do not form any triangle");
 		}
 	}
-
 }
