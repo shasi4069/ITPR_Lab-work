51ba7c2b66c47ad0da38993bad4019d3c0236aba
Shasi <shasi4069@gmail.com>
2025-12-01 10:00:49 +0530

    Add CourseController for managing course registrations
    Implements CourseController for course registration.

---
+++ CollegeManagement/Controller/CourseController.java
@@ -0,0 +1,71 @@
+package com.knmodi.college1.Controller;
+import java.util.Scanner;
+import com.knmodi.college1.Service.*;
+import com.knmodi.college1.Service.Impl.*;
+import com.knmodi.college1.model.*;
+
+
+public class CourseController {
+	
+
+	private CourseService courseService;
+	public CourseController()
+	{
+		//to initailize service
+		courseService = new CourseServiceImpl();
+	}
+	
+	
+	
+	
+	public void registerCourseUI()
+	{
+		Scanner sc = new Scanner(System.in);
+
+		String choice = "y";
+
+		do {
+		    System.out.print("Enter Course ID: ");
+		    String courseId = sc.nextLine();
+
+		    System.out.print("Enter Course Name: ");
+		    String courseName = sc.nextLine();
+
+		    System.out.print("Enter Course Description: ");
+		    String courseDesc = sc.nextLine();
+
+		    System.out.print("Enter Course Duration: ");
+		    int duration = sc.nextInt();
+
+		    System.out.print("Enter Course Min Enrollment: ");
+		    int minEnroll = sc.nextInt();
+
+		    System.out.print("Enter Course Max Enrollment: ");
+		    int maxEnroll = sc.nextInt();
+
+		    // consume leftover newline before nextLine()
+		    sc.nextLine();
+		    //calling register method
+		    courseService.registerCourse(new Course(courseId, courseName, courseDesc, duration, minEnroll, maxEnroll));
+		    
+		   
+
+		    System.out.println("Do You want to enter more data (y | n) :");
+		    choice = sc.nextLine();
+
+		} while (!choice.equalsIgnoreCase("n"));
+
+		sc.close();
+
+		
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
